a7c0f1fd7b50d38a6703627888a324a26ee0e79b
Ivan Rebrischev <ivbl21@ya.ru>
2023-05-19 14:51:25 +0300

    xml-config added

--- src/main/java/com/ivdev/spring/ApplicationRunner.java
+++ src/main/java/com/ivdev/spring/ApplicationRunner.java
@@ -5,25 +5,17 @@ import com.ivdev.spring.database.pool.ConnectionPool;
 import com.ivdev.spring.database.repository.CompanyRepository;
 import com.ivdev.spring.ioc.Container;
 import com.ivdev.spring.service.UserService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import javax.sql.ConnectionPoolDataSource;
 import java.awt.*;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
-        var container = new Container();
+        var context = new ClassPathXmlApplicationContext("app.xml");
 
-//        var connectionPool = new ConnectionPool();
-//        var userRepository = new UserRepository((ConnectionPoolDataSource) connectionPool);
-//        var companyRepository = new CompanyRepository((ConnectionPoolDataSource) connectionPool);
-//        var userService = new UserService(userRepository, companyRepository);
-
-//        var connectionPool = container.get(ConnectionPool.class);
-//        var userRepository = container.get(UserRepository.class);
-//        var companyRepository = container.get(CompanyRepository.class);
-
-        //тут происходит внедрение зависимостей, нам не нужно знать какие есть зависимости
-        //мы не управляем созданием UserService, это делает сторонний объект
-        var userService = container.get(UserService.class);
+        //use alias "pool2" to recognize required bean
+        var connectionPool = context.getBean("pool2", ConnectionPool.class);
+        System.out.println(connectionPool);
     }
 }
