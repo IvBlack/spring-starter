23c00718053ed800928c035009d968904d64cd72
Ivan Rebrischev <ivbl21@ya.ru>
2023-05-21 22:52:38 +0300

    constr mapped to xml

--- src/main/java/com/ivdev/spring/service/UserService.java
+++ src/main/java/com/ivdev/spring/service/UserService.java
@@ -8,8 +8,8 @@ public class UserService {
     private final UserRepository userRepository;
     private final CompanyRepository companyRepository;
 
-    //UserService не управляет созданием userRepository
-    //он внедрен через конструктор, создание и управление происходит извне
+    //UserService does not manage the creation of a UserRepository
+    //UserRepository is implemented through the constructor, creation and management takes place from the outside
     public UserService(UserRepository userRepository, CompanyRepository companyRepository) {
         this.userRepository = userRepository;
         this.companyRepository = companyRepository;
