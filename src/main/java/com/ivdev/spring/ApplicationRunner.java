72363c4f2e9e75748cd77b1ca11fd6df2a85e3a5
Ivan Rebrischev <ivbl21@ya.ru>
2023-06-06 18:12:38 +0300

    custom bfpp added

--- src/main/java/com/ivdev/spring/ApplicationRunner.java
+++ src/main/java/com/ivdev/spring/ApplicationRunner.java
@@ -5,13 +5,24 @@ import com.ivdev.spring.database.pool.ConnectionPool;
 import com.ivdev.spring.database.repository.CompanyRepository;
 import com.ivdev.spring.ioc.Container;
 import com.ivdev.spring.service.UserService;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import javax.sql.ConnectionPoolDataSource;
 import java.awt.*;
+import java.io.Serializable;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
+        //for demonstration
+        //with help of reflectionAPI, we can find out whether the given bean is a type of BeanFactoryPostProcessor or not
+        String value = "Ave, Spring";
+        System.out.println(CharSequence.class.isAssignableFrom(value.getClass()));
+        System.out.println(Serializable.class.isAssignableFrom(value.getClass()));
+        System.out.println(BeanFactoryPostProcessor.class.isAssignableFrom(value.getClass()));
+
+
+
         try (var context = new ClassPathXmlApplicationContext("app.xml"))
         {
             //use alias "p1" to recognize required bean
---
+++ src/main/java/com/ivdev/spring/bfpp/LogBeanFactoryPostProcessor.java
@@ -0,0 +1,18 @@
+package com.ivdev.spring.bfpp;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.PriorityOrdered;
+
+//PriorityOrdered has highest priority for benn queue
+public class LogBeanFactoryPostProcessor implements BeanFactoryPostProcessor, PriorityOrdered {
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {}
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
+    }
+}
