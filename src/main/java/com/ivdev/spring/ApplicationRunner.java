ec17d1d635b53a3a5af34001af0cfeebf5810827
Ivan Rebrischev <ivbl21@ya.ru>
2023-06-16 00:22:42 +0300

    bpp/TransactionBPP added

--- src/main/java/com/ivdev/spring/ApplicationRunner.java
+++ src/main/java/com/ivdev/spring/ApplicationRunner.java
@@ -3,6 +3,7 @@ package com.ivdev.spring;
 import com.ivdev.spring.database.UserRepository;
 import com.ivdev.spring.database.pool.ConnectionPool;
 import com.ivdev.spring.database.repository.CompanyRepository;
+import com.ivdev.spring.database.repository.CrudRepository;
 import com.ivdev.spring.ioc.Container;
 import com.ivdev.spring.service.UserService;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
@@ -29,6 +30,9 @@ public class ApplicationRunner {
             //clazz -> String -> Map(String, Object)
             var connectionPool = context.getBean("p1", ConnectionPool.class);
             System.out.println(connectionPool);
+
+            var companyRepository = context.getBean("companyRepository", CrudRepository.class);
+            System.out.println(companyRepository.findById(1));
         }
     }
 }
--- src/main/java/com/ivdev/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/ivdev/spring/database/repository/CompanyRepository.java
@@ -1,13 +1,35 @@
 package com.ivdev.spring.database.repository;
 
 import com.ivdev.spring.bpp.InjectBean;
+import com.ivdev.spring.bpp.Transaction;
+import com.ivdev.spring.database.entity.Company;
 import com.ivdev.spring.database.pool.ConnectionPool;
 
+import javax.annotation.PostConstruct;
 import javax.sql.ConnectionPoolDataSource;
+import java.util.Optional;
 
-public class CompanyRepository {
+@Transaction
+public class CompanyRepository implements CrudRepository<Integer, Company>{
 
     //custom annotation over field for inject bean
     @InjectBean
     private ConnectionPool connectionPool;
+
+    //to see how lifecycle bean works here
+    @PostConstruct
+    private void init() {
+        System.out.println("init company repository");
+    }
+
+    @Override
+    public Optional<Company> findById(Integer id) {
+        System.out.println("findById method..");
+        return Optional.of(new Company(id));
+    }
+
+    @Override
+    public void delete(Company Entity) {
+        System.out.println("delete method..");
+    }
 }
