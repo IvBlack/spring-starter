f6609add82a99ac8ec8a622f679a8d3dbf7d8d4e
Ivan R. <ivbl21@yandex.ru>
2023-12-05 19:08:49 +0300

    BaseEntity

--- src/main/java/com/ivdev/spring/database/entity/Company.java
+++ src/main/java/com/ivdev/spring/database/entity/Company.java
@@ -1,4 +1,21 @@
 package com.ivdev.spring.database.entity;
 
-public record Company(Integer id) {
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "company")
+public class Company implements BaseEntity<Integer>{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
 }
